5fea03e035ff308fcf388809e66041bb5e6b4525
Nitin Dogra <dogranitin579@gmail.com>
2026-02-10 17:41:05 +0530

    base url

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://logocraft-1.onrender.com",
+  baseURL: "https://logocraft-3.onrender.com",
   withCredentials: true, 
 });
 Api.interceptors.request.use(
