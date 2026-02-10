3d44b7834aa478fd3c7034efa1274c337e8dd279
Nitin Dogra <dogranitin579@gmail.com>
2026-02-10 17:10:47 +0530

    base url update

--- client/src/api/api.jsx
+++ client/src/api/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: "https://logocraft-4.onrender.com",
+  baseURL: "https://logocraft-1.onrender.com",
   withCredentials: true, 
 });
 Api.interceptors.request.use(
