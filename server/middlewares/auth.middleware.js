3be85d5fdcfebc4604090784c10569012b79fc2c
Nitin Dogra <dogranitin579@gmail.com>
2026-02-16 19:31:22 +0530

    cookie fix

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -8,7 +8,6 @@ export default function verifyUser(req, res, next) {
 
     const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-
     req.user = { id: decoded.userId };
     next();
   } catch (error) {
--- server/server.js
+++ server/server.js
@@ -19,8 +19,6 @@ app.use(
   cors({
     origin: ["https://logo-craft-q4ia.vercel.app"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
