3688a99f17d73df9e2e41be1d3cb65732d693992
Nitin Dogra <dogranitin579@gmail.com>
2026-02-21 03:02:36 +0530

    edit

--- client/src/api/loginApi.jsx
+++ client/src/api/loginApi.jsx
@@ -3,7 +3,6 @@ export const loginApi = async (input) => {
   try {
     const response = await Api.post("/auth/login", input);
     const token = await response.data.accessToken;
-    console.log(response.data);
     localStorage.setItem("token", token);
     localStorage.setItem("user", JSON.stringify(response.data.user));
     return response.data;
