76786c9793cc94a55aedc1fdd45ec4d5afc92943
Nitin Dogra <dogranitin579@gmail.com>
2026-03-27 00:58:58 +0530

    push

--- server/controllers/dashboard.controller.js
+++ server/controllers/dashboard.controller.js
@@ -1,12 +1,35 @@
 import User from "../models/user.model.js";
+import client from "../config/redis.js";
 
 export default async function dashboard(req, res) {
+  const userId = req.user.id;
+  const cacheKey = `user:${userId}`;
+
   try {
-    const userData = await User.findById(req.user.id)
-      .select("-password")
+    const cachedData = await client.get(cacheKey);
+
+    if (cachedData) {
+      console.log("Cache HIT");
+      return res.status(200).json(cachedData);
+    }
+
+    console.log("Cache MISS");
+
+    const userData = await User.findById(userId)
+      .select("-password -refreshToken")
       .lean();
 
-    res.status(200).json({ userData, message: "welcome to dashboard" });
+    const response = {
+      userData,
+    };
+
+    await client.setEx(cacheKey, 86400, JSON.stringify(response));
+
+    res.status(200).json({
+      response,
+      message: "welcome to dashboard",
+    });
+
   } catch (error) {
     console.log(error);
     res.status(400).json({ message: "user not found" });
